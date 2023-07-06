e16719d3e0966faae4b4f2666da819b24b6f3ba2
DurjoyAcharya <da-durjoy@outlook.com>
2023-07-06 11:57:22 +0600

    just stated

---
+++ src/test/java/com/app/sbend2endapplication/SbEnd2endApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.sbend2endapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbEnd2endApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
