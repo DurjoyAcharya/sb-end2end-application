79c681f831d5c778fa3fd134c4acd6d9a2089e42
DurjoyAcharya <da-durjoy@outlook.com>
2023-07-06 12:29:41 +0600

    just stated

--- src/main/java/com/app/sbend2endapplication/user/User.java
+++ src/main/java/com/app/sbend2endapplication/user/User.java
@@ -1,24 +1,36 @@
 package com.app.sbend2endapplication.user;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.NaturalId;
 
 import java.util.Collection;
 @Entity
 @Setter
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
+
+    public User(String firstName, String lastName, String userEmail, String password, Collection<Role> roles) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userEmail = userEmail;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    @NaturalId(mutable = true)
     private String userEmail;
     private String password;
     private boolean isEnable=false;
     @ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles",joinColumns = @JoinColumn(name = "user_id",
+    referencedColumnName = "id"
+    ), inverseJoinColumns = @JoinColumn(name = "role_id",referencedColumnName = "id"))
     private Collection<Role> roles;
 }
